003ea21fb4bc4c934523ddf4ec5bab7965f7f18d
violetorange <sergey.voronin04@gmail.com>
2025-01-28 20:06:29 +0300

    Main page pagination

--- assets/react/controllers/MainPage.jsx
+++ assets/react/controllers/MainPage.jsx
@@ -6,15 +6,18 @@ export default function MainArticlesList() {
     const [loading, setIsLoading] = useState(false);
     const [error, setError] = useState();
     const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+    const itemsPerPage = 9;
 
     useEffect(() => {
         const fetchArticles = async () => {
             setIsLoading(true);
 
             try {
-                const response = await fetch(`/api/articles.jsonld?isPublished=true&page=${page}`);
+                const response = await fetch(`/api/articles.jsonld?isPublished=true&page=${page}&itemsPerPage=${itemsPerPage}`);
                 const articles = await response.json();
                 setArticles(articles.member);
+                setTotalPages(Math.ceil(articles.totalItems / itemsPerPage));
             } catch (error) {
                 setError(error);
             } finally {
@@ -25,6 +28,10 @@ export default function MainArticlesList() {
         fetchArticles();
     }, [page]);
 
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+
     if (loading) {
         return <div>Загрузка...</div>;
     }
@@ -34,36 +41,46 @@ export default function MainArticlesList() {
     }
 
     return (
-        <Grid container justifyContent="center">
-            <Grid container item xs={12} sm={8} md={6} spacing={3} justifyContent="center">
-                {articles.map((article) => (
-                    <Grid item xs={12} sm={4} md={4} key={article["@id"]}>
-                        <Card
-                            style={{ width: '300px' }}
-                            sx={{
-                                width: '300px',
-                                transition: 'transform 0.3s, box-shadow 0.3s',
-                                '&:hover': {
-                                    transform: 'scale(1.05)', // Увеличение карточки при наведении
-                                    boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)', // Добавление тени
-                                }
-                            }}
-                        >
-                            <CardMedia
-                                component="img"
-                                height="200"
-                                image={article.imageUrl}
-                                alt={article.title}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h6">
-                                    {article.title}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
+        <div>
+            <Grid container justifyContent="center">
+                <Grid container item xs={12} sm={8} md={6} spacing={3} justifyContent="center">
+                    {articles.map((article) => (
+                        <Grid item xs={12} sm={4} md={4} key={article["@id"]}>
+                            <Card
+                                style={{ width: '300px' }}
+                                sx={{
+                                    width: '300px',
+                                    transition: 'transform 0.3s, box-shadow 0.3s',
+                                    '&:hover': {
+                                        transform: 'scale(1.05)', // Увеличение карточки при наведении
+                                        boxShadow: '0 4px 20px rgba(0, 0, 0, 0.2)', // Добавление тени
+                                    }
+                                }}
+                            >
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    image={article.imageUrl}
+                                    alt={article.title}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6">
+                                        {article.title}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Grid>
-        </Grid>
+            <Pagination
+                count={totalPages}
+                page={page}
+                onChange={handleChange}
+                variant="outlined"
+                shape="rounded"
+                sx={{ marginTop: 2, display: 'flex', justifyContent: 'center' }}
+            />
+        </div>
     );
 }
